42d5f404ed900aab58fea84c2ecf358e009aac1d
Renferal <f.croce93@gmail.com>
2017-03-15 13:39:04 +0100

    Custom Marker examples

--- app/src/main/java/com/project/pervsys/picaround/MapsActivity.java
+++ app/src/main/java/com/project/pervsys/picaround/MapsActivity.java
@@ -2,17 +2,31 @@ package com.project.pervsys.picaround;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, OnMarkerClickListener, GoogleMap.OnInfoWindowClickListener, GoogleMap.InfoWindowAdapter {
+
+    private static final LatLng PERTH = new LatLng(-31.952854, 115.857342);
+    private static final LatLng SYDNEY = new LatLng(-33.87365, 151.20689);
+    private static final LatLng BRISBANE = new LatLng(-27.47093, 153.0235);
 
     private GoogleMap mMap;
+
+    private Marker mPerth;
+    private Marker mSydney;
+    private Marker mBrisbane;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +52,78 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add some markers to the map, and add a data object to each marker.
+        mPerth = mMap.addMarker(new MarkerOptions()
+                .position(PERTH)
+                .title("Perth")
+                .snippet("This is how we show the snippet!:" +
+                        "\n" +
+                        "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\n" +
+                        "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB\n" +
+                        "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC\n" +
+                        "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD\n" +
+                        "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
+        mPerth.setTag(0);
+
+        mSydney = mMap.addMarker(new MarkerOptions()
+                .position(SYDNEY)
+                .title("Sydney")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+        mSydney.setTag(0);
+
+        mBrisbane = mMap.addMarker(new MarkerOptions()
+                .position(BRISBANE)
+                .title("Brisbane")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+        mBrisbane.setTag(0);
+
+        // Set a listener for marker click.
+        mMap.setOnMarkerClickListener(this);
+
+        // Set a listener for infoWindow click.
+        mMap.setOnInfoWindowClickListener(this);
+
+        // Set InfoWindowAdapter
+        mMap.setInfoWindowAdapter(this);
+    }
+
+    /** Called when the user clicks a marker. */
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+
+        // Retrieve the data from the marker.
+        Integer clickCount = (Integer) marker.getTag();
+
+        // Check if a click count was set, then display the click count.
+        if (clickCount != null) {
+            clickCount = clickCount + 1;
+            marker.setTag(clickCount);
+            Toast.makeText(this,
+                    marker.getTitle() +
+                            " has been clicked " + clickCount + " times.",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        // Return false to indicate that we have not consumed the event and that we wish
+        // for the default behavior to occur (which is for the camera to move such that the
+        // marker is centered and for the marker's info window to open, if it has one).
+        return false;
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        String toShow = marker.getPosition().toString();
+        Toast.makeText(this, toShow,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return new InfoWindowView(this);
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        return null;
     }
 }
