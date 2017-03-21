7dfaa2be6aefd42739ac4fd94e3f4473761623b8
Riccardo <distefanor3@gmail.com>
2017-03-21 12:52:15 +0100

    Add menu and remove logout button in MainActivity

--- app/src/main/java/com/project/pervsys/picaround/MainActivity.java
+++ app/src/main/java/com/project/pervsys/picaround/MainActivity.java
@@ -8,6 +8,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,6 +21,8 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.project.pervsys.picaround.utility.Config;
+
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,9 +51,64 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void onLogOutClickListener(View w) {
-        prepareLogOut();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        String logged = getSharedPreferences(Config.LOG_PREFERENCES, 0)
+                .getString(Config.LOG_PREF_INFO, null);
+        //if the user is not logged, then add login to the menu
+        if(logged != null && !logged.equals(Config.NOT_LOGGED))
+            menu.add(R.string.logout);
+        //if the user is logged, then add logout to the menu
+        else
+            menu.add(R.string.login);
+        return super.onCreateOptionsMenu(menu);
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.settings:
+                Log.i(TAG, "Settings has been selected");
+                Toast.makeText(this, "Selected settings", Toast.LENGTH_SHORT).show();
+                //Settings activity
+                return true;
+            case R.id.contact:
+                Log.i(TAG, "Contact has been selected");
+                Toast.makeText(this, "Selected contact", Toast.LENGTH_SHORT).show();
+                //Contact activity
+                return true;
+            case R.id.help:
+                Log.i(TAG, "Help has been selected");
+                Toast.makeText(this, "Selected help", Toast.LENGTH_SHORT).show();
+                //Help activity
+                return true;
+            case R.id.info:
+                Log.i(TAG, "Info has been selected");
+                Toast.makeText(this, "Selected info", Toast.LENGTH_SHORT).show();
+                //Info activity
+                return true;
+            default:
+                String title = (String) item.getTitle();
+                if (title.equals(getResources().getString(R.string.login))) {
+                    Log.i(TAG, "Login has been selected");
+                    Toast.makeText(this, "Selected login", Toast.LENGTH_SHORT).show();
+                    getSharedPreferences(Config.LOG_PREFERENCES, MODE_PRIVATE).edit()
+                            .putString(Config.LOG_PREF_INFO, Config.NOT_LOGGED).apply();
+                    startLogin();
+                    return true;
+                } else {
+                    Log.i(TAG, "Logout has been selected");
+                    Toast.makeText(this, "Selected logout", Toast.LENGTH_SHORT).show();
+                    prepareLogOut();
+                }
+        }
+        return false;
+    }
+
 
 
     private void prepareLogOut(){
@@ -80,21 +140,23 @@ public class MainActivity extends AppCompatActivity {
         }
         String logged = getSharedPreferences(Config.LOG_PREFERENCES,MODE_PRIVATE)
                 .getString(Config.LOG_PREF_INFO,null);
-        if (logged != null & logged.equals(Config.GOOGLE_LOGGED)){
-            Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
-                    new ResultCallback<Status>() {
-                        @Override
-                        public void onResult(Status status) {
-                            if (status.isSuccess()) {
-                                Log.i(TAG, "Logout from Google");
-                                getSharedPreferences(Config.LOG_PREFERENCES, MODE_PRIVATE).edit()
-                                        .putString(Config.LOG_PREF_INFO, Config.NOT_LOGGED).apply();
-                                ApplicationClass.setGoogleApiClient(null);
-                                startLogin();
-                            } else
-                                Log.e(TAG, "Error during the Google logout");
-                        }
-                    });
+        if (logged != null){
+            if (logged.equals(Config.GOOGLE_LOGGED)) {
+                Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                        new ResultCallback<Status>() {
+                            @Override
+                            public void onResult(Status status) {
+                                if (status.isSuccess()) {
+                                    Log.i(TAG, "Logout from Google");
+                                    getSharedPreferences(Config.LOG_PREFERENCES, MODE_PRIVATE).edit()
+                                            .putString(Config.LOG_PREF_INFO, Config.NOT_LOGGED).apply();
+                                    ApplicationClass.setGoogleApiClient(null);
+                                    startLogin();
+                                } else
+                                    Log.e(TAG, "Error during the Google logout");
+                            }
+                        });
+            }
         }
     }
 
