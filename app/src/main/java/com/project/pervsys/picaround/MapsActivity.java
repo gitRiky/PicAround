be355bafb59d896278f9e71ac3b4199bc2a27b3d
Riccardo <distefanor3@gmail.com>
2017-04-06 18:01:15 +0200

    Send photo to the storage and take the path for the db

--- app/src/main/java/com/project/pervsys/picaround/MapsActivity.java
+++ app/src/main/java/com/project/pervsys/picaround/MapsActivity.java
@@ -159,40 +159,7 @@ public class MapsActivity extends AppCompatActivity implements LocationListener,
         return f;
     }
 
-    private void setPic() {
 
-		/* There isn't enough memory to open up more than a couple camera photos */
-		/* So pre-scale the target bitmap into which the file is decoded */
-
-		/* Get the size of the ImageView */
-        int targetW = mImageView.getWidth();
-        int targetH = mImageView.getHeight();
-
-		/* Get the size of the image */
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        bmOptions.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
-        int photoW = bmOptions.outWidth;
-        int photoH = bmOptions.outHeight;
-
-		/* Figure out which way needs to be reduced less */
-        int scaleFactor = 1;
-        if ((targetW > 0) || (targetH > 0)) {
-            scaleFactor = Math.min(photoW/targetW, photoH/targetH);
-        }
-
-		/* Set bitmap options to scale the image decode target */
-        bmOptions.inJustDecodeBounds = false;
-        bmOptions.inSampleSize = scaleFactor;
-        bmOptions.inPurgeable = true;
-
-		/* Decode the JPEG file into a Bitmap*/
-        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
-
-
-        mImageView.setImageBitmap(bitmap);
-        mImageView.setVisibility(View.VISIBLE);
-    }
 
     private void galleryAddPic() {
         Intent mediaScanIntent = new Intent("android.intent.action.MEDIA_SCANNER_SCAN_FILE");
@@ -232,7 +199,6 @@ public class MapsActivity extends AppCompatActivity implements LocationListener,
 
         if (mCurrentPhotoPath != null) {
             Log.i(TAG, "The photo has been taken");
-            setPic();
             Log.i(TAG, "Set pic ok");
             galleryAddPic();
             Log.i(TAG, "Gallery pic ok");
@@ -297,9 +263,6 @@ public class MapsActivity extends AppCompatActivity implements LocationListener,
             }
         };
 
-
-        mImageView = (ImageView) findViewById(R.id.imageView);
-        mImageBitmap = null;
 
         mAlbumStorageDirFactory = new AlbumStorageDirFactory();
 
--- app/src/main/java/com/project/pervsys/picaround/UploadPhotoActivity.java
+++ app/src/main/java/com/project/pervsys/picaround/UploadPhotoActivity.java
@@ -2,6 +2,7 @@ package com.project.pervsys.picaround;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -14,20 +15,32 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
+
+import java.io.File;
 
 public class UploadPhotoActivity extends AppCompatActivity {
     private final static String TAG = "UploadPhotoActivity";
     private static final String PHOTO_PATH = "photoPath";
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
-
+    private StorageReference mStorageRef;
+    private ImageView mImageView;
+    private EditText nameField;
+    private EditText descriptionField;
     private String mPhotoPath;
+
 
 
     @Override
@@ -62,40 +75,16 @@ public class UploadPhotoActivity extends AppCompatActivity {
                 }
             }
         };
-
+        mStorageRef = FirebaseStorage.getInstance().getReference();
         mPhotoPath = getIntent().getStringExtra(PHOTO_PATH);
         Log.i(TAG, "Started activity, photo's path = " + mPhotoPath);
-        ImageView imageView = (ImageView) findViewById(R.id.image_to_upload);
-        /*Picasso.with(this)
-                .load(mPhotoPath)
-                .resize(10,10)
-                .into(imageView);*/
-        int targetW = imageView.getWidth();
-        int targetH = imageView.getHeight();
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        bmOptions.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(mPhotoPath, bmOptions);
-        int photoW = bmOptions.outWidth;
-        int photoH = bmOptions.outHeight;
-
-		/* Figure out which way needs to be reduced less */
-        int scaleFactor = 1;
-        if ((targetW > 0) || (targetH > 0)) {
-            scaleFactor = Math.min(photoW/targetW, photoH/targetH);
-        }
-
-		/* Set bitmap options to scale the image decode target */
-        bmOptions.inJustDecodeBounds = false;
-        bmOptions.inSampleSize = scaleFactor;
-        bmOptions.inPurgeable = true;
-        Bitmap bitmap = BitmapFactory.decodeFile(mPhotoPath, bmOptions);
-
-
-        imageView.setImageBitmap(bitmap);
-        imageView.setVisibility(View.VISIBLE);
-        Log.i(TAG, "Image loaded into ImageView");
-
+        mImageView = (ImageView) findViewById(R.id.image_to_upload);
+        nameField = (EditText) findViewById(R.id.photo_name);
+        descriptionField = (EditText) findViewById(R.id.photo_description);
+        setPic();
+        Log.i(TAG, "Photo put into the imageView");
     }
+
 
     @Override
     protected void onStart() {
@@ -125,12 +114,99 @@ public class UploadPhotoActivity extends AppCompatActivity {
         int id = item.getItemId();
         switch(id){
             case R.id.upload:
-                //TODO: check the correctness of data
                 Log.i(TAG, "Upload has been selected");
-                Toast.makeText(this, "Selected upload", Toast.LENGTH_SHORT).show();
-                //TODO: put the photo into the db
+                String name = nameField.getText().toString();
+                String description = descriptionField.getText().toString();
+                if(checkName(name) && checkDescription(description)) {
+                    Log.d(TAG, "Ready for sending data to db");
+                    Toast.makeText(this, "Selected upload", Toast.LENGTH_SHORT).show();
+                    //put the photo into the storage
+                    Uri file = Uri.fromFile(new File(mPhotoPath));
+                    StorageReference riversRef = mStorageRef.child(name);
+
+                    riversRef.putFile(file)
+                            .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                    // Get a URL to the uploaded content
+                                    Log.d(TAG, "DONEEEE");
+                                    getPath();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception exception) {
+                                    // Handle unsuccessful uploads
+                                    // ...
+                                    Log.d(TAG, "ERROR!");
+                                }
+                            });
+                    //TODO: close the activity with RESULT_OK
+                }
                 return true;
         }
         return false;
+    }
+
+
+    private void setPic() {
+
+		/* There isn't enough memory to open up more than a couple camera photos */
+		/* So pre-scale the target bitmap into which the file is decoded */
+
+		/* Get the size of the ImageView */
+        int targetW = mImageView.getWidth();
+        int targetH = mImageView.getHeight();
+
+		/* Get the size of the image */
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+		/* Figure out which way needs to be reduced less */
+        int scaleFactor = 1;
+        if ((targetW > 0) || (targetH > 0)) {
+            scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+        }
+
+		/* Set bitmap options to scale the image decode target */
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+		/* Decode the JPEG file into a Bitmap*/
+        Bitmap bitmap = BitmapFactory.decodeFile(mPhotoPath, bmOptions);
+
+
+        mImageView.setImageBitmap(bitmap);
+        mImageView.setVisibility(View.VISIBLE);
+    }
+
+    private boolean checkName(String name){
+        if (name.replace(" ", "").equals("")){
+            Toast.makeText(this, R.string.name_missing, Toast.LENGTH_SHORT).show();
+            nameField.setText("");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkDescription(String description){
+        //TODO: remove this if no check is needed
+        return true;
+    }
+
+    private void getPath(){
+        StorageReference pathRef = mStorageRef.child(nameField.getText().toString());
+
+        pathRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Log.d(TAG, "MyDownloadLink:  " + uri);
+                //TODO: put the path into the db
+            }
+        });
     }
 }
